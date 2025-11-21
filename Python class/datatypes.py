c5097317fca68024a4ed0b265c30bda5821128f4
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-11-21 12:36:22 +0530

    Python_Datatypes_EX1

---
+++ Python class/datatypes.py
@@ -0,0 +1,27 @@
+# print is function --takes a parameter and write to console
+
+print("Bio data")
+
+# input
+
+name = input("please enter your name:")
+print("Name "+ name)
+print(type(name))
+
+age = input("please enter your age:")
+age = int(age)
+print(age)
+phone = input("please enter your phone no:")
+print(type(phone))
+graduate = input("please enter your degree:")
+print("Degree " + graduate)
+salary = input("please enter your salary_expection:")
+print("salary_expection " + salary)
+
+## Type Convention
+# Afer a year 
+age=int(age)
+age = age + 1
+print(age)
+print (type(age))
+
