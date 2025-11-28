31eab1cefe28b57cd4c70da0d9201473b9729550
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-11-28 12:09:24 +0530

    msg

---
+++ Python class/Loop.py
@@ -0,0 +1,34 @@
+# for loop & While loop 
+# what is different b/w while & for 
+
+# Continuous in life 
+# eg auto renuwal - billing generate 
+# clock 
+# count down timer 
+# heart beat 
+# passing the value in function is parameter while calling the function it's agrument 
+
+# while - we need termination but for clock example we no need terminate because it's indefinite
+
+# while expression:
+#      statement 1
+# 
+
+seconds = 1
+
+while True:
+    print(seconds)
+    seconds = seconds + 1
+
+    if seconds > 60:
+        break
+
+# while seconds < 60:
+#     print (seconds)
+#     seconds = seconds + 1
+
+def print_update(num):
+    pass
+
+
+
