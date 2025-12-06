ee3f2702b09ab05d7404dbfb56e4174bab26c37e
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-06 11:26:51 +0530

    list

--- Python class/list.py
+++ Python class/list.py
@@ -4,7 +4,7 @@
 
 # list -> list of numbers, list of floats, list of strings, etc... -> composite data types
 
-# print(dir(list)) ## list functions 
+#print(dir(list)) ## list functions 
 
 ## 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort'
 
@@ -33,34 +33,36 @@ old_list = [3,4,5,6]
 new_list = old_list.copy()
 print(new_list)
 
-# 4.count() -- Counts how many times a value appears.
+# # 4.count() -- Counts how many times a value appears.
 text = [1,"a",2,"b","a",3,2,4,1]
-print(text.count("a"))
+print(text.count(3))
 
-# 5.extend -- Adds elements of another list to the end.
-# ad=[1,2,3]
-# add_list=ad.extend([0,7])
-# print(add_list)
+# # 5.extend -- Adds elements of another list to the end.
+ad=[1,2,3]
+add_list=ad.extend([0,7])
+print(ad)
 
 # 6.index - Returns the index of the first occurrence of a value.
 ads=[1,2,3,2,4]
 print(ads.index(2))
 
-# 7.insert(index,value) # insert the value in given index value 
+# # 7.insert(index,value) # insert the value in given index value 
 ads=[1,2,3,2,4]
 ads.insert(2,9) 
 
-print(ads)
+#  print(ads)
 
 #8.pop() -- remove the last value
-re=ads.pop()
+re=ads.pop() # parameter is index value -- pop (index value )
 print(re)
 
-# 9.remove() - Removes the first occurrence of a value.
-# a =num.remove(3)
-# print(a)
+# 9.remove() - Removes the first occurrence of a value. -- remove the value in parameter
+print(num)
+a =num.remove(3)
+print(a)
+print(num)
 
-# 10.reverse() - Reverses the list in-place.
+# 10.reverse() - Reverses the list in-place. -- modify the value in same memory location 
 q = [1,2,3,4,5]
 print(q)
 q.reverse()
@@ -72,4 +74,9 @@ print(asc)
 asc.sort()
 print(asc)
 
-# 
+# length of list
+print(len(num))
+
+#sum 
+print(sum(num))
+# print(sum(text))
