ef33e3c4954ee0160dfd65f88a56071c7603637d
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-11 09:23:57 +0530

    List, set & tuple difference

---
+++ Python class/List_set_tuple.py
@@ -0,0 +1,14 @@
+# List -- no limits
+# Ordered collection with out any limitation 
+# square brackets []
+# mutable
+
+# Set  -- no duplicates
+# un ordered collection
+# {} brackets
+# mutable
+
+# Tuple -- immutable
+# no edit/modify 
+# ordered collection 
+
