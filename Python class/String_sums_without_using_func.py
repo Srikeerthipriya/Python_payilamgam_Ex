cfdfad6782878b7f1cf48ba5fd6f59adba6930d3
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-17 07:21:51 +0530

    string sums without using function

---
+++ Python class/String_sums_without_using_func.py
@@ -0,0 +1,12 @@
+#   i/p - i am going to school
+#   O/p - I am going to School
+
+sen = "i am going to school"
+wrds =[]
+
+for i in sen:
+    #print(i)
+    if (i):
+        wrds.append(i)
+
+print(wrds)
