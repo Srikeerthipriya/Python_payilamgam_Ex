b329bfc9173a159de69cd740470234b57362a028
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-17 11:44:28 +0530

    test code

--- Python class/test_code.py
+++ Python class/test_code.py
@@ -1,12 +1,32 @@
-num2 = [1,2,3,4,5]
-rotate = 2
-for i in range(rotate):
+# n = [1,2,3,4,5]
+# rotate = 2
+# for i in range(rotate):
 
-    li=len(num2)-1
-    z=num2[li]
+#     li=len(n)-1
+#     z=n[li]
 
-    for x in range(li,0,-1):
-        num2[x]=num2[x-1]
-    num2[0]=z
+#     for x in range(li,0,-1):
+#         n[x]=n[x-1]
+#     n[0]=z
 
-print("2nd rotation:",num2)
+# print("2nd rotation:",n)
+
+# n = [1,2,3,4,5]
+
+# # for 2 rotation clock wise 
+
+# a = n[3:]
+# b = n[0:3]
+
+# print("clock wise 2rotate using sciling",a+b)
+
+# # clock wise 2rotate using sciling
+
+# a = n[0:1]
+# b = n[1:5:1]
+
+# print("clock wise 2rotate using sciling:",b+a)
+
+
+
+print(dir(list)) 
