69e7750aa21494c8f8f0ee0dda349d852308abbf
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-18 11:39:59 +0530

    list comprehension

---
+++ Python class/List_comprehension.py
@@ -0,0 +1,14 @@
+# list comprehension 
+
+n = [1,2,3,4,5,6]
+
+even = [i for i in n if(i%2==0)] # i -- my output result
+
+# Syntax of list comprehension
+# 1) i -- my output result
+# 2) loops
+# 3) condition 
+
+# list comprehension only provide one o/p
+
+print("even no",even)
