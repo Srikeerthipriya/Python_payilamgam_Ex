e49bdd650cf8dc711541b0b26935792dd689ed40
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-18 11:40:19 +0530

    set,list,tuple

--- Python class/List_set_tuple.py
+++ Python class/List_set_tuple.py
@@ -2,11 +2,13 @@
 # Ordered collection with out any limitation 
 # square brackets []
 # mutable
+# duplicate can be allowed 
 
 # Set  -- no duplicates
 # un ordered collection
 # {} brackets
 # mutable
+# eg -- signal ={red,green,yellow}
 
 # Tuple -- immutable
 # no edit/modify 
