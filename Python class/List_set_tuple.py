6d0023264641d2e6d5122de50d97c8c7c4fbebd6
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-22 11:49:36 +0530

    list, set,dic

--- Python class/List_set_tuple.py
+++ Python class/List_set_tuple.py
@@ -10,6 +10,16 @@
 # mutable
 # eg -- signal ={red,green,yellow}
 
+# Dictionary - { }
+# dic = {"key": value}
+# mutuable 
+# unique kyes 
+# methods - get,update
+# get(key)
+# update(key:values) -- if the key is already there it will update or else it will insert new key 
+
+
+
 # Tuple -- immutable
 # no edit/modify 
 # ordered collection 
