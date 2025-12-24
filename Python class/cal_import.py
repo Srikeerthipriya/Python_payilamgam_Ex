9e97bd14b0d1f9d49269be01cf90cd02ea4079b5
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-24 11:59:14 +0530

    import the file to other file

---
+++ Python class/cal_import.py
@@ -0,0 +1,11 @@
+
+# module
+
+# syntax -- from module-(filename) import function_name
+from calculator import add 
+
+# syntax -- from package.module import function_name
+# package -- folder name & module-(filename)
+
+x=add(20,80)
+print(x)
