cbcdaa26bbb9c5a24d37835c904131fd2a4b9f31
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2025-12-25 11:35:59 +0530

    function

--- Python class/calculator.py
+++ Python class/calculator.py
@@ -12,3 +12,29 @@ def div(x, y):
 
 def power(x, y):
   return x ** y
+
+def output(action,units):
+    global quantity,sold
+    print("___")
+    print("no of pencil :",action,units)
+    print("Total remaining no of quantity:",quantity)
+    print("Total no of sold:",sold)
+    print("___")
+
+quantity = 100
+price = 10
+sold = 0
+
+def sell(no_of_quanty):
+    """
+    Docstring for sell
+    
+    :param no_of_quanty: # sell no of units of pencil 
+    # quantity will reduce by no of units
+    # sold will increase by no of units
+    
+    """
+    global quantity,sold
+    quantity = quantity - no_of_quanty
+    sold = sold + no_of_quanty
+    return no_of_quanty
