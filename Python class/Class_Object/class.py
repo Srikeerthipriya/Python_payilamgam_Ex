d2a493b5b91b2368ec3acaba8a0b6dc89d00306b
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2026-01-06 12:02:18 +0530

    class & object

---
+++ Python class/Class_Object/class.py
@@ -0,0 +1,36 @@
+class employee: # declaring the class 
+    
+    def __init__(self,first_name,last_name,salary):
+        self.first = first_name  ## first 
+        self.last = last_name 
+        self.salary = salary
+
+    def name(self): # we are calling the full -- must give the parameter self
+        print(self.first,self.last)
+
+    def increment(self,value):
+        self.salary = self.salary + value
+        print(self.salary)
+
+
+# object / instance of employee -- e_one 
+e_one= employee ("Keerthi","Shankar",50000) # here only we are creating the class employee 
+# print(e_one.first,e_one.last) ## first the name should be name 
+e_one.name() #we no need to pass the parameter it will call automaticatlly
+e_one.increment(10000)
+
+
+e_two = employee("Nikshi","P",60000)
+#print(e_two.first,e_two.last)
+e_two.name()
+e_two.increment(10000)
+
+
+
+# employee() -- instantiation / constructed 
+#print(type(e_one))
+
+# o/p - <class '__main__.employee'> 
+# -- main -- envinorment (currently which file we running) 
+#  employee -- class name 
+
