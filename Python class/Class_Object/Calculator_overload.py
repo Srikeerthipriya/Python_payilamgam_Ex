4d97c60ad52ec9ad4c9e73d2358b1e4fec47bcfe
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2026-01-12 12:09:55 +0530

    calculator overload in same class

---
+++ Python class/Class_Object/Calculator_overload.py
@@ -0,0 +1,19 @@
+# class method overload in same class 
+
+# try the overload in child class 
+# variable length org 
+
+class Calculator:
+
+    def add(x,y):
+        pass
+
+    def add(x,y,z):
+        pass
+
+    def mul(x,y):
+        pass
+
+    def mul(x,y,z):
+        pass
+
