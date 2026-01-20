dbd70ea500de5d6186f6f00a869131c94b48b2a7
Srikeerthi2401 <srikeerthipriyashankar@gmail.com>
2026-01-20 10:57:21 +0530

    read the fany file

---
+++ Python class/File/read_file.py
@@ -0,0 +1,9 @@
+file = open("D:\Payilagam\Product.csv")
+
+# print(file.read()) ## read all the content 
+
+print(file.readline()) ## read the 1st line
+
+print(file.readlines()) ## read all the lines in list 
+
+file.close() # closing the file 
